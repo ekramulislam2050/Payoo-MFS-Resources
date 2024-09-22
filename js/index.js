830aeb591828c15767af68d908d2ba3e87cf31a9
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-09-22 21:20:10 +0600

    recheack

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const loginButton = document.getElementById("btn-login");
     const passwordField = document.getElementById("password-field");
     const passwordFieldValue = passwordField.value ;
     if(numberFieldValue === '0' && passwordFieldValue === '9'){
-      window.location.href='/index2.html';
+      window.location.href='index2.html';
       console.log(numberFieldValue,passwordFieldValue)
     }
     else{
